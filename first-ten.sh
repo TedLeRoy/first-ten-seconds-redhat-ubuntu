f2779c1c0e27cbae3fbd8a2106567fcebf9ad91e
Ted LeRoy <ted.leroy@protonmail.com>
2020-09-21 05:08:43 +0000

    Finished Ubuntu

--- first-ten.sh
+++ first-ten.sh
@@ -18,9 +18,11 @@ normal=$( tput sgr 0 );
 osName=`cat /etc/*os-release | grep ^NAME | cut -d '"' -f 2`
 
 # Checking if running as root. If yes, asking to change to a non-root user.
+# This verifies that a non-root user is configured and is being used to run
+# the script.
+
 if [ ${UID} == 0  ]
 then
-
   echo "${red}
   You're running this script as root user.
   Please configure a non-root user and run this
@@ -31,9 +33,14 @@ then
   exit
 fi
 
+#################################################
+#                 Ubuntu Section                #
+#################################################
+
+# If OS is Ubuntu, apply the security settings for Ubuntu
+
 if [ "$osName" == "Ubuntu" ]
 then
-
   echo "${green}  You're running "$osName" Linux. "$osName" security
   first measures will be applied.
 
@@ -41,59 +48,106 @@ then
   Please enter it when asked.
   ${normal}
   "
-
-  echo "${yellow}  Enabling ufw firewall. It will allow only ssh inbound.
+  # Enabling ufw firewall and making sure it allows SSH
+  echo "${yellow}  Enabling ufw firewall. Ensuring SSH is allowed.
   ${normal}"
   sudo ufw allow ssh
   sudo ufw --force enable
   echo "${green}
   Done configuring ufw firewall.
   ${normal}"
-
-  if [ -f /home/$USER/.ssh/authorized_keys ]
-  then
-  echo "${yellow}  
-  Locking down SSH so it will only permit key-based authentication.
-  ${normal}"
-  echo -n "${red}  
-    Are you sure you want to allow only key-based authentication for SSH? 
-    PASSWORD AUTHENTICATIN WILL BE DISABLED FOR SSH ACCESS!
-    (y or n)${normal}" 
-    read answer
-    
-    if [ "$answer" == "y" ] ;then
-
-      echo "${yellow}
-      Adding the following lines to a file in sshd_config.d
+    # Checking whether an authorized_keys file exists in logged in user's account.
+    # If so, the assumption is that key based authentication is set up.
+    if [ -f /home/$USER/.ssh/authorized_keys ]
+    then
+      echo "${yellow}  
+      Locking down SSH so it will only permit key-based authentication.
       ${normal}"
-
-      echo "DebianBanner no
+      echo -n "${red}  
+      Are you sure you want to allow only key-based authentication for SSH? 
+      PASSWORD AUTHENTICATIN WILL BE DISABLED FOR SSH ACCESS!
+      (y or n)${normal}" 
+      read answer
+      # Putting relevant lines in /etc/ssh/sshd_config.d/11-sshd-first-ten.conf file
+      if [ "$answer" == "y" ] ;then
+        echo "${yellow}
+        Adding the following lines to a file in sshd_config.d
+        ${normal}"
+        echo "DebianBanner no
 DisableForwarding yes
 PermitRootLogin no
 IgnoreRhosts yes
 PasswordAuthentication no
-PermitEmptyPasswords no" | sudo tee -a /etc/ssh/sshd_config.d/11-sshd-first-ten.conf 
-      echo "${yellow}
-      Reloading ssh
-      ${normal}"
-      sudo systemctl reload ssh
-      echo "${green}
-      ssh has been restarted.
-      ${normal}"
+PermitEmptyPasswords no" | sudo tee /etc/ssh/sshd_config.d/11-sshd-first-ten.conf 
+        echo "${yellow}
+        Reloading ssh
+        ${normal}"
+	# Restarting ssh daemon
+        sudo systemctl reload ssh
+        echo "${green}
+        ssh has been restarted.
+        ${normal}"
 
       else
-      
-      echo "${red}
-      You have chosen not to disable password based authentication at this time.
-      Please do so yourself or re-run this script when you're prepared to do so.
-      ${normal}"
-    fi
+	# User chose a key other than "y" for configuring ssh so it will not be set up now
+        echo "${red}
+        You have chosen not to disable password based authentication at this time.
+        Please do so yourself or re-run this script when you're prepared to do so.
+        ${normal}"
+      fi
 
   else
+    # The check for an authorized_keys file failed so it is assumed key based auth is not set up
+    # Skipping this configuration and warning user to do it for herself
     echo "${red}  
-  It looks like SSH is not configured to allow key based authentication.
-  Please enable it and re-run this script.${normal}"
+    It looks like SSH is not configured to allow key based authentication.
+    Please enable it and re-run this script.${normal}"
   fi
+  # Installing fail2ban and networking tools (includes netstat)
+  echo "${yellow}
+  Installing fail2ban and networking tools.
+  ${normal}"
+  sudo apt install fail2ban net-tools -y
+  echo "${green}
+  fail2ban and networking tools have been installed.
+  ${normal}"
+  # Setting up the fail2ban jail for SSH
+  echo "${yellow}
+  Configuring fail2ban to protect SSH.
+
+  Entering the following into /etc/fail2ban/jail.local
+  ${normal}"
+  echo "# Default banning action (e.g. iptables, iptables-new,
+# iptables-multiport, shorewall, etc) It is used to define
+# action_* variables. Can be overridden globally or per
+# section within jail.local file
+
+[ssh]
+
+enabled  = true
+banaction = iptables-multiport
+port     = ssh
+filter   = sshd
+logpath  = /var/log/auth.log
+maxretry = 5
+findtime = 43200
+bantime = 86400" | sudo tee /etc/fail2ban/jail.local
+  # Restarting fail2ban
+  echo "${green}
+  Restarting fail2ban
+  ${normal}"
+  sudo systemctl restart fail2ban
+  # Tell the user what the protections are set to
+  echo "${green}
+  fail2ban is now protecting SSH with the following defaults:
+  maxretry: 5
+  findtime: 12 hours (43200 seconds)
+  bantime: 24 hours (86400 seconds)
+  ${normal}"
+
+#################################################
+#          CentOS / Red Hat Section             #
+#################################################
 
 elif [ "$osName" == "CentOS Linux" ] || [ "$osName" == "Red Hat Enterprise Linux" ]
 then
